58a78e1f40e5d235f00162369602cca71ead98d9
Kishor Gangurde <kishorgangurde100@gmail.com>
2026-06-07 12:54:02 +0000

    date_dimention_two_macro

--- macros/date_utils.sql
+++ macros/date_utils.sql
@@ -5,3 +5,16 @@ case when to_timestamp({{x}})  < current_date
 end
 
 {% endmacro %}
+
+
+{% macro function2(y) %}
+CASE 
+WHEN MONTH(TO_TIMESTAMP({{y}})) IN (12,1,2)
+THEN 'winter'
+WHEN MONTH(TO_TIMESTAMP({{y}})) IN (3,4,5)
+THEN 'spring'
+WHEN MONTH(TO_TIMESTAMP({{y}})) IN (6,7,8)
+THEN 'summer'
+ELSE 'Atumn'
+end
+{% endmacro %}
--- models/example/date_dimention.sql
+++ models/example/date_dimention.sql
@@ -1,29 +1,13 @@
 WITH CTE AS 
 (
 SELECT
-
 TO_TIMESTAMP(STARTED_AT) as started_at,
 DATE(TO_TIMESTAMP(STARTED_AT)) AS DATE_STARTED_AT,
 HOUR(TO_TIMESTAMP(STARTED_AT)) AS HOUR_STARED_AT,
-CASE
-WHEN DAYNAME(TO_TIMESTAMP(STARTED_AT)) IN ('Sat','Sun')
-THEN 'Weekend'
-ELSE 'working'
-end AS day_name,
-Month(TO_TIMESTAMP(STARTED_AT)),
 
-CASE 
-WHEN MONTH(TO_TIMESTAMP(STARTED_AT)) IN (12,1,2)
-THEN 'winter'
-WHEN MONTH(TO_TIMESTAMP(STARTED_AT)) IN (3,4,5)
-THEN 'spring'
-WHEN MONTH(TO_TIMESTAMP(STARTED_AT)) IN (6,7,8)
-THEN 'summer'
-ELSE 'Atumn'
-END as station_of_year,
+{{function2('started_at')}} as Season,
 
-{{function1('started_at')}}
-
+{{function1('started_at')}} as new
 FROM
 {{ source('demo1', 'bike') }}
 where STARTED_AT != 'started_at'
