4431ceb0ffe5bac640eb5b907069dfb19a24d04f
Kishor Gangurde <kishorgangurde100@gmail.com>
2026-06-07 06:09:59 +0000

    final_date_dimention_module

--- models/example/date_dimention.sql
+++ models/example/date_dimention.sql
@@ -1,10 +1,30 @@
+WITH CTE AS(
 
-WITH CTE AS (
-    SELECT
-        ride_id,
-        started_at
-    FROM {{ source('demo1', 'bike') }}
+SELECT
+started_at,
+TO_TIMESTAMP(started_at),
+DATE(TO_TIMESTAMP(STARTED_AT)) as DATE_STARTED_AT,
+HOUR(TO_TIMESTAMP(STARTED_AT)) as HOUR_STARTED_AT,
+CASE
+ WHEN DAYNAME(TO_TIMESTAMP(STARTED_AT)) IN ('SAT','SUN')
+THEN 'WEEKEND'
+ELSE 'BUSINESSSDAY'
+END AS DAY_TYPE,
+
+CASE WHEN MONTH(TO_TIMESTAMP(STARTED_AT)) IN (12,1,2)
+     THEN 'WINTER'
+     WHEN MONTH(TO_TIMESTAMP(STARTED_AT)) IN (3,4,5)
+     THEN 'SPRING'
+     WHEN MONTH(TO_TIMESTAMP(STARTED_AT)) IN (6,7,8)
+     THEN 'SUMMER'
+     ELSE 'AUTUMN'
+     END AS STATION_OF_YEAR
+
+from
+{{ source('demo1', 'bike') }}
+where started_at != 'started_at'
+
 )
 
-SELECT *
-FROM CTE
+SELECT 
+* FROM CTE
